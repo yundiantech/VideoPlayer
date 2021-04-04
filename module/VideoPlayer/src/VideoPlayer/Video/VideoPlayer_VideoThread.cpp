65a92af97aabbbe66bca44bd316ec9de8049a739
huihui <214517703@qq.com>
2021-04-04 11:49:24 +0800

    Update VideoPlayer_VideoThread.cpp

--- module/VideoPlayer/src/VideoPlayer/Video/VideoPlayer_VideoThread.cpp
+++ module/VideoPlayer/src/VideoPlayer/Video/VideoPlayer_VideoThread.cpp
@@ -15,31 +15,31 @@ static int autorotate = 1;
 static int find_stream_info = 1;
 static int filter_nbthreads = 0;
 
-static const struct TextureFormatEntry {
-    enum AVPixelFormat format;
-    int texture_fmt;
-} sdl_texture_format_map[] = {
-    { AV_PIX_FMT_RGB8,           SDL_PIXELFORMAT_RGB332 },
-    { AV_PIX_FMT_RGB444,         SDL_PIXELFORMAT_RGB444 },
-    { AV_PIX_FMT_RGB555,         SDL_PIXELFORMAT_RGB555 },
-    { AV_PIX_FMT_BGR555,         SDL_PIXELFORMAT_BGR555 },
-    { AV_PIX_FMT_RGB565,         SDL_PIXELFORMAT_RGB565 },
-    { AV_PIX_FMT_BGR565,         SDL_PIXELFORMAT_BGR565 },
-    { AV_PIX_FMT_RGB24,          SDL_PIXELFORMAT_RGB24 },
-    { AV_PIX_FMT_BGR24,          SDL_PIXELFORMAT_BGR24 },
-    { AV_PIX_FMT_0RGB32,         SDL_PIXELFORMAT_RGB888 },
-    { AV_PIX_FMT_0BGR32,         SDL_PIXELFORMAT_BGR888 },
-    { AV_PIX_FMT_NE(RGB0, 0BGR), SDL_PIXELFORMAT_RGBX8888 },
-    { AV_PIX_FMT_NE(BGR0, 0RGB), SDL_PIXELFORMAT_BGRX8888 },
-    { AV_PIX_FMT_RGB32,          SDL_PIXELFORMAT_ARGB8888 },
-    { AV_PIX_FMT_RGB32_1,        SDL_PIXELFORMAT_RGBA8888 },
-    { AV_PIX_FMT_BGR32,          SDL_PIXELFORMAT_ABGR8888 },
-    { AV_PIX_FMT_BGR32_1,        SDL_PIXELFORMAT_BGRA8888 },
-    { AV_PIX_FMT_YUV420P,        SDL_PIXELFORMAT_IYUV },
-    { AV_PIX_FMT_YUYV422,        SDL_PIXELFORMAT_YUY2 },
-    { AV_PIX_FMT_UYVY422,        SDL_PIXELFORMAT_UYVY },
-    { AV_PIX_FMT_NONE,           SDL_PIXELFORMAT_UNKNOWN },
-};
+//static const struct TextureFormatEntry {
+//    enum AVPixelFormat format;
+//    int texture_fmt;
+//} sdl_texture_format_map[] = {
+//    { AV_PIX_FMT_RGB8,           SDL_PIXELFORMAT_RGB332 },
+//    { AV_PIX_FMT_RGB444,         SDL_PIXELFORMAT_RGB444 },
+//    { AV_PIX_FMT_RGB555,         SDL_PIXELFORMAT_RGB555 },
+//    { AV_PIX_FMT_BGR555,         SDL_PIXELFORMAT_BGR555 },
+//    { AV_PIX_FMT_RGB565,         SDL_PIXELFORMAT_RGB565 },
+//    { AV_PIX_FMT_BGR565,         SDL_PIXELFORMAT_BGR565 },
+//    { AV_PIX_FMT_RGB24,          SDL_PIXELFORMAT_RGB24 },
+//    { AV_PIX_FMT_BGR24,          SDL_PIXELFORMAT_BGR24 },
+//    { AV_PIX_FMT_0RGB32,         SDL_PIXELFORMAT_RGB888 },
+//    { AV_PIX_FMT_0BGR32,         SDL_PIXELFORMAT_BGR888 },
+//    { AV_PIX_FMT_NE(RGB0, 0BGR), SDL_PIXELFORMAT_RGBX8888 },
+//    { AV_PIX_FMT_NE(BGR0, 0RGB), SDL_PIXELFORMAT_BGRX8888 },
+//    { AV_PIX_FMT_RGB32,          SDL_PIXELFORMAT_ARGB8888 },
+//    { AV_PIX_FMT_RGB32_1,        SDL_PIXELFORMAT_RGBA8888 },
+//    { AV_PIX_FMT_BGR32,          SDL_PIXELFORMAT_ABGR8888 },
+//    { AV_PIX_FMT_BGR32_1,        SDL_PIXELFORMAT_BGRA8888 },
+//    { AV_PIX_FMT_YUV420P,        SDL_PIXELFORMAT_IYUV },
+//    { AV_PIX_FMT_YUYV422,        SDL_PIXELFORMAT_YUY2 },
+//    { AV_PIX_FMT_UYVY422,        SDL_PIXELFORMAT_UYVY },
+//    { AV_PIX_FMT_NONE,           SDL_PIXELFORMAT_UNKNOWN },
+//};
 
 #if CONFIG_AVFILTER
 int VideoPlayer::configure_filtergraph(AVFilterGraph *graph, const char *filtergraph, AVFilterContext *source_ctx, AVFilterContext *sink_ctx)
@@ -105,7 +105,7 @@ double get_rotation(AVStream *st)
 
 int VideoPlayer::configure_video_filters(AVFilterGraph *graph, const char *vfilters, AVFrame *frame)
 {
-    enum AVPixelFormat pix_fmts[FF_ARRAY_ELEMS(sdl_texture_format_map)];
+//    enum AVPixelFormat pix_fmts[FF_ARRAY_ELEMS(sdl_texture_format_map)];
     char sws_flags_str[512] = "";
     char buffersrc_args[256];
     int ret;
@@ -127,7 +127,7 @@ int VideoPlayer::configure_video_filters(AVFilterGraph *graph, const char *vfilt
 //            }
 //        }
 //    }
-    pix_fmts[nb_pix_fmts] = AV_PIX_FMT_NONE;
+//    pix_fmts[nb_pix_fmts] = AV_PIX_FMT_NONE;
 
 //    while ((e = av_dict_get(sws_dict, "", e, AV_DICT_IGNORE_SUFFIX)))
 //    {
@@ -169,8 +169,8 @@ int VideoPlayer::configure_video_filters(AVFilterGraph *graph, const char *vfilt
     if (ret < 0)
         goto fail;
 
-    if ((ret = av_opt_set_int_list(filt_out, "pix_fmts", pix_fmts,  AV_PIX_FMT_NONE, AV_OPT_SEARCH_CHILDREN)) < 0)
-        goto fail;
+//    if ((ret = av_opt_set_int_list(filt_out, "pix_fmts", pix_fmts,  AV_PIX_FMT_NONE, AV_OPT_SEARCH_CHILDREN)) < 0)
+//        goto fail;
 
     last_filter = filt_out;
 
